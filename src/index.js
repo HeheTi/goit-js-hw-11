fc38d499c9ab87a65f380ce0c3dcaf374f52a561
Tetiana Zykova <tatiana.nv96@gmail.com>
2021-11-22 22:01:01 +0200

    add start files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+
+import './sass/main.scss';
---
+++ src/js/api.js
@@ -0,0 +1,2 @@
+API_KEY = '24468918 - f1629215ca3337ba51b4044a7';
+BASE_URL = 'https://pixabay.com/api/';
